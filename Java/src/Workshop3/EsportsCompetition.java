f985eb9d421a5be84d76fb1623c0aeb4a9ea0ffe
aaditya6882 <aadityaacharya6882@gmail.com>
2025-12-03 19:38:01 +0545

    Workshop3

---
+++ Java/src/Workshop3/EsportsCompetition.java
@@ -0,0 +1,26 @@
+package Workshop3;
+
+public class EsportsCompetition extends Competition {
+	private String teamName;
+	public EsportsCompetition(String competitionName, String teamName) {
+		super(competitionName);
+		this.teamName = teamName;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	@Override
+	public void getFullDetails(Competitor competitor) {
+		System.out.println("Name = " + competitor.getName());
+		System.out.println("Level = " + competitor.getLevel());
+		System.out.println("Country = " + competitor.getCountry());
+		System.out.println("Score = " + competitor.getOverallScore());
+		System.out.println("Competition Name = " + super.getCompetitionName());
+		System.out.println("Team = " + teamName);
+	}
+
+}
---
+++ Java/src/Workshop3/Name.java
@@ -0,0 +1,28 @@
+package Workshop3;
+
+public class Name {
+	private final String firstName;
+	private final String lastName;
+
+	public Name(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return   firstName +" "+ lastName;
+	}
+	
+}
